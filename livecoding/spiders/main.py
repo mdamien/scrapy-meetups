330af2b78a4fba8f656ce260f2a9899b5f172d71
Damien <damien@dam.io>
2015-10-05 23:58:04 -0700

    play more with the data (map via meetup locations) and scrap past events

--- livecoding/spiders/main.py
+++ livecoding/spiders/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.selector import Selector
+import json
 
 def doc_title(response):
     return response.selector.xpath('//title/text()').extract()[0] \
@@ -53,6 +55,8 @@ class MeetupSpider(scrapy.Spider):
             yield scrapy.Request(url.extract(), self.parse_event)
         for url in response.css('.nearby-description a::attr("href")'):
             yield scrapy.Request(url.extract(), self.parse)
+        yield scrapy.Request(response.url+'events/past/?page=0&__fragment=centerColMeetupList',
+                callback=lambda r:self.parse_past_events(r, 0))
         #scrapy.Request(response.url+'members/', self.parse_members_list)
 
     def parse_event(self, response):
@@ -63,7 +67,10 @@ class MeetupSpider(scrapy.Spider):
         else:
             start = None
         title = response.css('#event-title h1::text').extract()[0]
-        description = response.css('#event-description-wrap').extract()[0]
+        description = response.css('#event-description-wrap')
+        if len(description) == 0:
+            description = response.css('#past-event-description-wrap')
+        description = description.extract()[0]
         attendees = list({x for x in \
                 response.css('.event-attendees a::attr("href")').extract()})
         yield {
@@ -77,6 +84,21 @@ class MeetupSpider(scrapy.Spider):
         }
         for url in attendees:
             yield scrapy.Request(url, self.parse_member)
+
+    def parse_past_events(self, response, i):
+        try:
+            body = json.loads(response.body_as_unicode())[0]
+        except:
+            return
+        links = Selector(text=body).css('a::attr("href")').extract()
+        for url in links:
+            if '/members/' in url:
+                yield scrapy.Request(url, self.parse_member)
+            elif '/events/' in url and not '/past/' in url:
+                yield scrapy.Request(url, self.parse_event)
+        if len(links) > 0:
+            yield scrapy.Request(response.url+'events/past/?page={}&__fragment=centerColMeetupList'.format(i),
+                    callback=lambda r:self.parse_past_events(r, i+1))
 
     def parse_members_list(self, response):
         for url in response.css('.memName::attr("href")'):
